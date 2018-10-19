30903c3988c6a729f4a40236c526412f29fbc5a2
Kyle M Hall <kyle.m.hall@gmail.com>
2018-10-19 12:07:00 -0400

    Update bootstrap.sh

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,1 +1,6 @@
-docker run -d --restart=always -p 3000:4000  --mount type=bind,source=/usr/share/koha/lib/,target=/kohalib --mount type=bind,source=/etc/koha/sites/$INSTANCE/koha-conf.xml,target=/koha-conf.xml --mount type=bind,source=/etc/koha/sites/$INSTANCE/ncip-config.yml,target=/app/config.yml --name koha-ncip-$INSTANCE bywater/koha-ncip-server:latest
+docker run -d --restart=always -p 3000:4000 \
+              --mount type=bind,source=/usr/share/koha/lib/,target=/kohalib \
+              --mount type=bind,source=/etc/koha/sites/$INSTANCE/koha-conf.xml,target=/koha-conf.xml \
+              --mount type=bind,source=/etc/koha/sites/$INSTANCE/ncip-config.yml,target=/app/config.yml \
+              --mount type=bind,source=/var/run/mysqld/mysqld.sock,target=/var/run/mysqld/mysqld.sock \
+              --name koha-ncip-$INSTANCE bywater/koha-ncip-server:latest
