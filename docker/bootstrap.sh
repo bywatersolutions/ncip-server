20c9faaed1749cfbdee6c07e6441d19aac153c8d
Kyle M Hall <kyle.m.hall@gmail.com>
2018-10-19 12:08:47 -0400

    Update bootstrap.sh

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$INSTANCE" ]]; then
+    echo "You must set the environment variable INSTANCE to a Koha instance before running this script" 1>&2
+    exit 1
+fi
+
 docker run -d --restart=always -p 3000:4000 \
               --mount type=bind,source=/usr/share/koha/lib/,target=/kohalib \
               --mount type=bind,source=/etc/koha/sites/$INSTANCE/koha-conf.xml,target=/koha-conf.xml \
